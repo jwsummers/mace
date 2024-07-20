da91fa2abb2dabfbbe00c95cc0229c8b40045d66
Jason Summers <jwsummers.dev@gmail.com>
2024-07-19 22:14:41 -0400

    redesign hero content and mobile responsive nav

--- scripts.js
+++ scripts.js
@@ -4,21 +4,37 @@ function scrollToContact() {
 
 // Ensure the document is fully loaded
 document.addEventListener("DOMContentLoaded", function() {
-    // Get all nav links
     const navLinks = document.querySelectorAll("nav ul li a");
 
-    // Add click event to each nav link
     navLinks.forEach(link => {
         link.addEventListener("click", function(event) {
-            event.preventDefault(); // Prevent default anchor click behavior
-            const targetId = this.getAttribute("href").substring(1); // Get the target section ID
-            const targetSection = document.getElementById(targetId); // Get the target section element
-
-            // Scroll to the target section
+            event.preventDefault();
+            const targetId = this.getAttribute("href").substring(1);
+            const targetSection = document.getElementById(targetId);
             targetSection.scrollIntoView({
                 behavior: "smooth"
             });
         });
     });
-});
 
+    const navLinksContainer = document.querySelector('.nav-links');
+    const menuIcon = document.querySelector('.menu-icon');
+    const navItems = document.querySelectorAll('.nav-links li a');
+
+    function toggleMenu() {
+        navLinksContainer.classList.toggle('show');
+    }
+
+    function closeMenu() {
+        navLinksContainer.classList.remove('show');
+    }
+
+    menuIcon.addEventListener('click', toggleMenu);
+
+    // Optionally close the menu when the mouse leaves the nav links container
+    navLinksContainer.addEventListener('mouseleave', closeMenu);
+
+    navItems.forEach(item => {
+        item.addEventListener('click', closeMenu);
+    });
+});
