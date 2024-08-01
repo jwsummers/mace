18be4dcbc426a604440a87fa06a458e033f1d5f9
Jason Summers <jwsummers.dev@gmail.com>
2024-07-31 22:08:35 -0400

    reload scripts

--- scripts.js
+++ scripts.js
@@ -1,3 +1,56 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll("nav ul li a");
+
+    navLinks.forEach(link => {
+        link.addEventListener("click", function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute("href").substring(1);
+            const targetSection = document.getElementById(targetId);
+            targetSection.scrollIntoView({
+                behavior: "smooth"
+            });
+        });
+    });
+
+    const navLinksContainer = document.querySelector('.nav-links');
+    const menuIcon = document.querySelector('.menu-icon');
+    const navItems = document.querySelectorAll('.nav-links li a');
+
+    function toggleMenu() {
+        navLinksContainer.classList.toggle('show');
+    }
+
+    function closeMenu() {
+        navLinksContainer.classList.remove('show');
+    }
+
+    menuIcon.addEventListener('click', toggleMenu);
+
+    // Optionally close the menu when the mouse leaves the nav links container
+    navLinksContainer.addEventListener('mouseleave', closeMenu);
+
+    navItems.forEach(item => {
+        item.addEventListener('click', closeMenu);
+    });
+
+    // Flip card functionality
+    const infoButtons = document.querySelectorAll('.info-button');
+    infoButtons.forEach(button => {
+        button.addEventListener('click', function(event) {
+            event.stopPropagation();
+            const serviceCard = this.closest('.service-card');
+            serviceCard.classList.add('flipped');
+        });
+    });
+
+    const serviceCards = document.querySelectorAll('.service-card');
+    serviceCards.forEach(card => {
+        card.addEventListener('mouseleave', function() {
+            this.classList.remove('flipped');
+        });
+    });
+});
+
 document.getElementById('contact-form').addEventListener('submit', function (event) {
     event.preventDefault();
 
